dd18caaacfe84cf294b1bea5c2f13f6fac0908c9
chan2slo <chan2slo@naver.com>
2024-04-17 04:18:23 +0900

    test commit

--- newsfeeder_bot.py
+++ newsfeeder_bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('안녕하세요! 에코 봇입니다.')
+    await update.message.reply_text('안녕하세요! 에코 봇입니당!!!!!!')
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
